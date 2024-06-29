076da4d6117e13bb5c8b347b196fc82ca4635630
Nitin Tiwari <111339364+furiouscommentary@users.noreply.github.com>
2024-06-29 11:01:43 +0530

    auth middleware created

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import userModel from "../models/user.model.js";
+import { jwt } from jsonwebtoken;
+
+
+
+const verifyJWT = asynHandler( async (req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        if(!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await userModel.findById(decodedToken?._id).select("-password -refreshToken")
+
+        
+    } catch(error) {
+
+    }
+})
+
+
+export { verifyJWT };
